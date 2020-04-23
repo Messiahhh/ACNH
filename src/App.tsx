d9091e4531ca9e765c8124133e2a8c73295c4648
messiahhh <616348105@qq.com>
2020-04-24 00:04:45 +0800

    手动更新鱼类的中文名

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Button } from 'antd'
 import './App.css';
 
+import json from './api/Fish.json'
+
 function App() {
-  return (
-    <div className="App">
-      Welcome to ACNH Handbook
-      <Button>测试</Button>
-    </div>
-  );
+    return (
+        <div className="app">
+            {json.map((item) => 
+                <div key={item.id}>{item.name_zh}</div>
+            )}
+        </div>
+    )
 }
 
 export default App;
