9539270b89b0a57f0550d329c0d1d0675070de90
messiahhh <616348105@qq.com>
2020-04-26 00:21:39 +0800

    唉

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 import React from 'react'
 import Fish from './components/fish'
-import {
-    useSelector,
-    useDispatch,
-} from 'react-redux'
+import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 import 'antd/dist/antd.css' 
 import './index.scss'
 
-// interface appState {
-//     month: string,
-//     place: string,
-//     size: string
-// }
+interface RootState {
+    month: string,
+    place: string,
+    size: string,
+}
+
 
 function App() {
-    // let myState: appState = useSelector(state => state)
+    let state = useSelector((state: RootState) => state)
     return (
         <div className="app">
-            {/* {myState.month} */}
-            
+            {state.place}
             <Fish></Fish>
         </div>
     )
 }
+
 
 export default App
