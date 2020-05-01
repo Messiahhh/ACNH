6cf692ccab08739794ecb6086bd828c381b07248
messiahhh <616348105@qq.com>
2020-05-02 01:14:48 +0800

    搬砖

--- src/components/Villagers.tsx
+++ src/components/Villagers.tsx
@@ -10,6 +10,186 @@ import {
     Button,
 } from 'antd'
 import json from '../api/json/villagers.json'
+
+const personality: any = {
+    Cranky: {
+        text: '暴躁',
+        value: 'Cranky',
+    },
+    Lazy: {
+        text: '悠闲',
+        value: 'Lazy',
+    },
+    Smug: {
+        text: '自恋',
+        value: 'Smug',
+    },
+    Jock: {
+        text: '运动',
+        value: 'Jock',
+    },
+    Normal: {
+        text: '普通',
+        value: 'Normal',
+    },
+    Snooty: {
+        text: '成熟',
+        value: 'Snooty',
+    },
+    Peppy: {
+        text: '元气',
+        value: 'Peppy',
+    },
+    Uchi: {
+        text: '大姐姐',
+        value: 'Uchi',
+    },
+}
+
+const species: any = {
+    Anteater: {
+        text: '食蚁兽',
+        value: 'Anteater',
+    },
+    Bear: {
+        text: '大熊',
+        value: 'Bear',
+    },
+    Bird: {
+        text: '小鸟',
+        value: 'Bird',
+    },
+    Bull: {
+        text: '牛',
+        value: 'Bull',
+    },
+    Cat: {
+        text: '猫',
+        value: 'Cat',
+    },
+    Cub: {
+        text: '小熊',
+        value: 'Cub',
+    },
+    Chicken: {
+        text: '小鸡',
+        value: 'Chicken',
+    },
+    Cow: {
+        text: '奶牛',
+        value: 'Cow',
+    },
+    Alligator: {
+        text: '鳄鱼',
+        value: 'Alligator',
+    },
+    Deer: {
+        text: '鹿',
+        value: 'Deer',
+    },
+    Dog: {
+        text: '狗',
+        value: 'Dog',
+    },
+    Duck: {
+        text: '鸭子',
+        value: 'Duck'
+    },
+    Elephant: {
+        text: '大象',
+        value: 'Elephant'
+    },
+    Frog: {
+        text: '青蛙',
+        value: 'Frog'
+    },
+    Goat: {
+        text: '山羊',
+        value: 'Goat',
+    },
+    Gorilla: {
+        text: '猩猩',
+        value: 'Gorilla'
+    },
+    Hamster: {
+        text: '仓鼠',
+        value: 'Hamster'
+    },
+    Hippo: {
+        text: '河马',
+        value: 'Hippo',
+    },
+    Horse: {
+        text: '马',
+        value: 'Horse'
+    },
+    Koala: {
+        text: '考拉',
+        value: 'Koala'
+    },
+    Kangaroo: {
+        text: '袋鼠',
+        value: 'Kangaroo'
+    },
+    Lion: {
+        text: '狮子',
+        value: 'Lion'
+    },
+    Monkey: {
+        text: '猴子',
+        value: 'Monkey'
+    },
+    Mouse: {
+        text: '老鼠',
+        value: 'Mouse'
+    },
+    Octopus: {
+        text: '章鱼',
+        value: 'Octopus'
+    },
+    Ostrich: {
+        text: '鸵鸟',
+        value: 'Ostrich'
+    },
+    Eagle: {
+        text: '老鹰',
+        value: 'Eagle'
+    },
+    Penguin: {
+        text: '企鹅',
+        value: 'Penguin'
+    },
+    Pig: {
+        text: '猪',
+        value: 'Pig'
+    },
+    Rabbit: {
+        text: '兔子',
+        value: 'Rabbit'
+    },
+    Rhino: {
+        text: '犀牛',
+        value: 'Rhino'
+    },
+    Sheep: {
+        text: '绵羊',
+        value: 'Sheep'
+    },
+    Squirrel: {
+        text: '松鼠',
+        value: 'Squirrel'
+    },
+    Tiger: {
+        text: '老虎',
+        value: 'Tiger'
+    },
+    Wolf: {
+        text: '狼',
+        value: 'Wolf'
+    }
+
+}
+
 const columns: any[] = [
     {
         title: "名称",
@@ -18,7 +198,6 @@ const columns: any[] = [
             return (
                 <div >
                     <img style={{ width: 50}}src={require(`../static/icons/villagers/${record["file-name"]}.png`)} alt=""/>
-                    
                     <span>{value['name-cn']}</span>
                 </div>
             )
@@ -34,69 +213,10 @@ const columns: any[] = [
         title: "性格",
         dataIndex: 'personality',
         render: (value: string) => {
-            switch (value) {
-                case 'Cranky':
-                    return '暴躁'
-                    break
-                case 'Lazy':
-                    return '悠闲'
-                    break
-                case 'Smug':
-                    return '自恋'
-                    break
-                case 'Jock':
-                    return '运动'
-                    break
-                case 'Normal':
-                    return '普通'
-                    break
-                case 'Snooty':
-                    return '成熟'
-                    break
-                case 'Peppy':
-                    return '元气'
-                    break
-                case 'Uchi':
-                    return '大姐姐'
-                    break
-                default:
-                    return value
-                    break;
-            }
+            return personality[value].text
         },
         filters: [
-            {
-                text: '暴躁',
-                value: 'Cranky'
-            },
-            {
-                text: '悠闲',
-                value: 'Lazy'
-            },
-            {
-                text: '自恋',
-                value: 'Smug'
-            },
-            {
-                text: '运动',
-                value: 'Jock'
-            },
-            {
-                text: '普通',
-                value: 'Normal'
-            },
-            {
-                text: '成熟',
-                value: 'Snooty'
-            },
-            {
-                text: '元气',
-                value: 'Peppy'
-            },
-            {
-                text: '大姐姐',
-                value: 'Uchi'
-            },
+            ...Object.values(personality)
         ],
         onFilter: (value: string, record: any) => {
             return record.personality === value
@@ -109,11 +229,34 @@ const columns: any[] = [
     },
     {
         title: "生日",
-        dataIndex: 'birthday-string'
+        dataIndex: 'birthday-string',
+        sorter: (a: any, b: any) => {
+            const [a_month, a_day] = a.birthday.split('/').map((i: string) => Number(i))
+            const [b_month, b_day] = b.birthday.split('/').map((i: string) => Number(i))
+            
+            if (a_day !== b_day) return a_day - b_day 
+            else {
+                return a_month - b_month
+            }
+        }
     },
     {
         title: '种族',
         dataIndex: 'species',
+        render: (value: string) => {
+            return species[value].text
+        },
+        filters: [
+            ...Object.values(species)
+        ],
+        onFilter: (value: string, record: any) => {
+            return record.species === value
+        },
+        sorter: (a: any, b: any) => {
+            const char1 = a.species.charCodeAt(0)
+            const char2 = b.species.charCodeAt(0)
+            return char1 - char2
+        }
     },
     {
         title: '性别',
