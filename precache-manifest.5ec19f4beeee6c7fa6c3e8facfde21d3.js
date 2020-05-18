00b81e4eda890b9050f0f2c50be94e1c089a752d
messiahhh <616348105@qq.com>
2020-05-18 12:49:15 +0800

    deploy

---
+++ precache-manifest.5ec19f4beeee6c7fa6c3e8facfde21d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d6d012765b773a707968566c328c4d",
+    "url": "/acnh/index.html"
+  },
+  {
+    "revision": "9c119686e3d82bc67919",
+    "url": "/acnh/static/css/0.34b05c93.chunk.css"
+  },
+  {
+    "revision": "e2f55fefdc5254a0b255",
+    "url": "/acnh/static/css/1.3d43f092.chunk.css"
+  },
+  {
+    "revision": "a9e6332170ed60f0aa6d",
+    "url": "/acnh/static/css/4.bfcef66f.chunk.css"
+  },
+  {
+    "revision": "7708f02e0bef580e47fd",
+    "url": "/acnh/static/css/main.bd8d0575.chunk.css"
+  },
+  {
+    "revision": "9c119686e3d82bc67919",
+    "url": "/acnh/static/js/0.1d2a5cf3.chunk.js"
+  },
+  {
+    "revision": "e2f55fefdc5254a0b255",
+    "url": "/acnh/static/js/1.ab53b89b.chunk.js"
+  },
+  {
+    "revision": "a9e6332170ed60f0aa6d",
+    "url": "/acnh/static/js/4.5cc00215.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/acnh/static/js/4.5cc00215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1cba91e6489dd51b6bd",
+    "url": "/acnh/static/js/5.787c4ee9.chunk.js"
+  },
+  {
+    "revision": "d79085b2285a250de3ac",
+    "url": "/acnh/static/js/6.36dd5eed.chunk.js"
+  },
+  {
+    "revision": "481ffc1f471580a054da",
+    "url": "/acnh/static/js/7.86dff950.chunk.js"
+  },
+  {
+    "revision": "151e694c900ab4f22df1",
+    "url": "/acnh/static/js/8.7e8553bc.chunk.js"
+  },
+  {
+    "revision": "c5768c6a63e9ffd3ed4d",
+    "url": "/acnh/static/js/9.4b039742.chunk.js"
+  },
+  {
+    "revision": "7708f02e0bef580e47fd",
+    "url": "/acnh/static/js/main.f8be0f48.chunk.js"
+  },
+  {
+    "revision": "ad6e0d9cb88d72318ed5",
+    "url": "/acnh/static/js/runtime-main.3909b637.js"
+  },
+  {
+    "revision": "d6a22b7f4ae0275ddcabf94227fa4eae",
+    "url": "/acnh/static/media/disc.d6a22b7f.jpg"
+  },
+  {
+    "revision": "73ec264b5dadfc0933314f4b2f865a72",
+    "url": "/acnh/static/media/flowers.73ec264b.jpg"
+  },
+  {
+    "revision": "f5ddcf9de81bc573ddb25c4c6c07da98",
+    "url": "/acnh/static/media/footer-illustration.f5ddcf9d.svg"
+  },
+  {
+    "revision": "e833b2adc27c4e54e0075a2542a27b52",
+    "url": "/acnh/static/media/fossil.e833b2ad.jpg"
+  },
+  {
+    "revision": "3af775e704601ce250cbb5611af42b22",
+    "url": "/acnh/static/media/villagers.3af775e7.png"
+  }
+]);
